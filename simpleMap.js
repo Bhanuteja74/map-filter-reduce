f5893ac573dee1fe2f6bb11527a45044d21c7287
Bhanu Teja Kommi <145745009+Bhanuteja74@users.noreply.github.com>
2024-12-22 18:24:13 +0530

    adds map lengthsOf

--- simpleMap.js
+++ simpleMap.js
@@ -6,8 +6,15 @@ const squaresOf = function (numbers) {
   return numbers.map(square);
 };
 
+// lengths of ["apple", "banana", "kiwi"] => [5, 6, 4]
+const stringLength = (string) => string.length;
+const lengthsOf = function (strings) {
+  return strings.map(stringLength);
+};
+
 // --------- test Cases --------
 testCases.push([squaresOf, [0, 1, 2, 3, 4], squaresOf([0, 1, 2, 3, 4]), [0, 1, 4, 9, 16]]);
+testCases.push([lengthsOf, ['a', 'ab', '', 'abc'], lengthsOf(['a', 'ab', '', 'abc']), [1, 2, 0, 3]]);
 
 // --------- failed test Cases ------
 //[function Name,  list,  Expected, Actual]
