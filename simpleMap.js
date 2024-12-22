e30a82e0b924b5b8b971666dd710e4476f266fa6
Bhanu Teja Kommi <145745009+Bhanuteja74@users.noreply.github.com>
2024-12-22 18:12:15 +0530

    adds map squares

--- simpleMap.js
+++ simpleMap.js
@@ -1,24 +1,24 @@
 const testCases = [];
 
 // squares of [1, 2, 3] => [1, 4, 9]
+const square = (x) => x * x;
 const squaresOf = function (numbers) {
-
+  return numbers.map(square);
 };
 
+// --------- test Cases --------
+testCases.push([squaresOf, [0, 1, 2, 3, 4], squaresOf([0, 1, 2, 3, 4]), [0, 1, 4, 9, 16]]);
 
 // --------- failed test Cases ------
 //[function Name,  list,  Expected, Actual]
 const failed = [];
 
 testCases.forEach(function ([fun, input, expected, actual]) {
-
   if (actual !== expected) {
     failed.push({
-      FUNCTION: fun, 'GIVEN LIST': input,
-      EXPECTED: expected, ACTUAL: actual
+      FUNCTION: fun, 'GIVEN LIST': input, EXPECTED: expected, ACTUAL: actual
     });
   }
-
 });
 
 if (failed.length === 0) {
