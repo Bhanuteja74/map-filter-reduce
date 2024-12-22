20e65a774ad47d359dfdaf54806fe08b9d03dccf
Bhanu Teja Kommi <145745009+Bhanuteja74@users.noreply.github.com>
2024-12-22 18:45:04 +0530

    adds map firstCharactersOf

--- simpleMap.js
+++ simpleMap.js
@@ -18,10 +18,17 @@ const uppercaseOf = function (strings) {
   return strings.map(upperCase);
 };
 
+// first characters of ["apple", "banana", "kiwi"] => ["a", "b", "k"]
+const firstChar = (string) => string.at(0);
+const firstCharactersOf = function (strings) {
+  return strings.map(firstChar);
+};
+
 // --------- test Cases --------
 testCases.push([squaresOf, [0, 1, 2, 3, 4], squaresOf([0, 1, 2, 3, 4]), [0, 1, 4, 9, 16]]);
 testCases.push([lengthsOf, ['a', 'ab', '', 'abc'], lengthsOf(['a', 'ab', '', 'abc']), [1, 2, 0, 3]]);
 testCases.push([uppercaseOf, ['a', 'ab', '', 'abc'], uppercaseOf(['a', 'ab', '', 'abc']), ['A', 'AB', '', 'ABC']]);
+testCases.push([firstCharactersOf, ['abc', 'a', ''], firstCharactersOf(['abc', 'a', '']), ['a', 'a', '']]);
 
 // --------- failed test Cases ------
 //[function Name,  list,  Expected, Actual]
